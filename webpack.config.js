3bd073ecf26c185bf2433aa633d6c458f65d4251
Pravin Suresh <selfstudy210619@gmail.com>
2025-10-03 14:06:43 +0530

    add output

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry:"./src/index.js"
+    entry:"./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true
+    }
 }
